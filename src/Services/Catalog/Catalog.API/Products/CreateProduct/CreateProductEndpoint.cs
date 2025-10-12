c2d54ccdf95b12d39d61ea1163cf73376cc2c20b
senerdag <mrsener48tr@gmail.com>
2025-10-13 00:43:17 +0300

    Catalog Service Initial

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+{ }
+
+public record CreateProductResponse(Guid Id)
+{ }
+public class CreateProductEndpoint
+{
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the container.
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+
 
 app.Run();
