9cf89af77bb3e45f6d5f2c76ce8a142aa6552d3a
senerdag <mrsener48tr@gmail.com>
2025-10-15 03:08:54 +0300

    register carter  mapster and mediatr

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -7,6 +7,24 @@ public record CreateProductRequest(string Name, List<string> Category, string De
 
 public record CreateProductResponse(Guid Id)
 { }
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+    }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,9 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container.
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapCarter();
 
 app.Run();
