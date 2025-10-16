25a5a77594417a15e223948642bdba18082b03f6
senerdag <mrsener48tr@gmail.com>
2025-10-17 01:50:02 +0300

    Marten rgs

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,3 +1,8 @@
 ﻿global using Carter;
 global using MediatR;
 global using Mapster;
+global using Marten;
+global using BuildingBlocks.CQRS;
+
+global using Catalog.API.Models;
+
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,14 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using MediatR;
-using System.Windows.Input;
-
+﻿
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult> 
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+
             var product = new Product
             {
                 Name = command.Name,
@@ -21,7 +18,11 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price,
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
 
 
             
